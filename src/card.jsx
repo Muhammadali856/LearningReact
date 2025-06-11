1ec1b85b338a4d978007a7cbc3b127f6d952a199
MUHAMMADALI-USER <eshboevmuhammadali2@gmail.com>
2025-06-11 18:37:26 +0500

    learned card component

---
+++ src/card.jsx
@@ -0,0 +1,13 @@
+import profilePic from './assets/cat-picture-profile.webp';
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src={profilePic} alt="profile picture" style={{ width: '220px', height: '150px'}}></img>
+            <h2 className="card-title">Muhammadali</h2>
+            <p className="card-text" >I am studying React from "Bro Code"</p>
+        </div>
+    );
+}
+
+export default Card;
