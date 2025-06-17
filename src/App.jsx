abcfaf03728cb4bdac3ff55f00f2be652cd6be8d
MUHAMMADALI-USER <eshboevmuhammadali2@gmail.com>
2025-06-17 18:30:06 +0500

    learned rendering list (2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import List from './List.jsx';
 
 function App() {
+
+  const fruits = [
+        { id: 1, name: "apple", calories: 95 },
+        { id: 2, name: "banana", calories: 105 },
+        { id: 3, name: "cherry", calories: 50 },
+        { id: 4, name: "coconut", calories: 159 },
+        { id: 5, name: "grape", calories: 62 }
+    ];
+
+    const vegetables = [
+        { id: 6, name: "carrot", calories: 25 },
+        { id: 7, name: "broccoli", calories: 55 },
+        { id: 8, name: "spinach", calories: 23 },
+        { id: 9, name: "potato", calories: 163 },
+        { id: 10, name: "tomato", calories: 22 }
+    ];
+
   return (
     <>
-      <List/>
+      {fruits.length > 0 ? <List items={fruits} category="Fruits"/> : null} {/* This list will not shown because list has arrived as a null list*/}
+      {vegetables.length > 0 ? <List items={vegetables} category="Vegetables"/> : null}
     </>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,28 +1,19 @@
 
-function List (){
-    
-    const fruits = [
-        { id: 1, name: "apple", calories: 95 },
-        { id: 2, name: "banana", calories: 105 },
-        { id: 3, name: "cherry", calories: 50 },
-        { id: 4, name: "coconut", calories: 159 },
-        { id: 5, name: "grape", calories: 62 }
-    ];
+function List ({ items = [], category = "Category" }){ // <-- Changes are here!
 
-    fruits.sort((a, b) => b.calories - a.calories);
-    
-    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100); // How to filter fruits with less than 100 calories
-    // const highCalFruits = fruits.filter(fruit => fruit.calories >= 100); // How to filter fruits with 100 or more calories
 
-    const itemList = fruits.map(fruit => <li key={fruit.id}>
-                                                {fruit.name}:&nbsp;
-                                                <b>{fruit.calories}</b> calories
+    const sortedItems = [...items].sort((a, b) => a.calories - b.calories);
+
+    const listItems = sortedItems.map(item => <li key={item.id}>
+                                            {item.name}:&nbsp;
+                                            <b>{item.calories}</b> calories
                                           </li>);
 
     return(
-        <div className="List">
-            <ol>{itemList}</ol>
-        </div>
+        <>
+            <h3 className="list-category">{category}</h3>
+            <ol className="list-items">{listItems}</ol>
+        </>
     )
 }
 
